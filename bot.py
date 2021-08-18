a7ed6a326f91396bed796bb0e947124889eaf022
rsilva42 <rsilva@student.42.us.org>
2021-08-17 21:45:51 -0400

    add discord api setup

--- bot.py
+++ bot.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
+import os
 import discord
+from dotenv import load_dotenv
 
 if __name__ == "__main__":
   pass
+
+load_dotenv()
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('aptis-bot is online')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  print(message.author)
+
+client.run(os.getenv('TOKEN'))
